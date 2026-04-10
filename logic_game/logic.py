c6d3ef57689034b04c51c6e1a6352db1751a8a0a
batya2281 <gleb.dmitrovskiy@gmail.com>
2026-04-10 21:01:37 +0300

    Добавил список используемых слов

--- logic_game/logic.py
+++ logic_game/logic.py
@@ -1,13 +1,16 @@
 class LogicGame:
-    def start_game(selfg):
+    def start_game(self):
         while True:
+            used_words = []
             print("Welcome to Logic Game")
             player1 = input("Player 1 name: ")
             player2 = input("Player 2 name: ")
             if player2[-1] == player1[-1]:
+                used_words.append(player1)
                 continue
 
             if player1[-1] == player2[-1]:
+                used_words.append(player2)
                 continue
 
             if player2[-1] != player1[-1]:
@@ -18,8 +21,9 @@ class LogicGame:
                 print("Player 2 fuck your ass!")
                 break
 
-            if player2 == player1:
+            if player2 in used_words:
+
                 print("Такое слово уже было")
 
-            if player1 == player2:
+            if player1 in used_words:
                 print("Такое слово уже было")
